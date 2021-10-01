d59190731c064bca86126d3bbcfd1b4d40374e97
Philipp <philipp.niemetz@gmail.com>
2021-10-01 16:08:45 +0200

    update

--- backend/src/controllers/rest.controller.ts
+++ backend/src/controllers/rest.controller.ts
@@ -4,7 +4,7 @@ import Track from "../models/track";
 
 const dbService: DbService = new DbService();
 
-export async function getAllPlaylists(req: Request, res: Response) {
+export async function getPlaylists(req: Request, res: Response) {
     console.log('GET /api/playlists');
 
     res.status(200).json(dbService.playlists);
@@ -18,4 +18,10 @@ export async function getPlaylistTracks(req: Request, res: Response) {
 
     const tracks: Track[] = dbService.getPlaylistTracks(playlistId);
     res.status(200).json(tracks);
+}
+
+export async function getGenres(req: Request, res: Response) {
+    console.log('GET /api/genres')
+
+    res.status(200).json(dbService.genres);
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getAllPlaylists, getPlaylistTracks } from "./controllers/rest.controller";
+import { getGenres, getPlaylists, getPlaylistTracks } from "./controllers/rest.controller";
 
 export const router = express.Router();
 
-router.get('/playlists', getAllPlaylists);
+router.get('/playlists', getPlaylists);
 router.get('/playlisttracks/:id', getPlaylistTracks);
-router.get('/genres');
+router.get('/genres', getGenres);
 router.get('/tracks?genreid=:id');
 router.post('/track');
 router.delete('/track?playlistid=:playListid&trackid=:trackid');
